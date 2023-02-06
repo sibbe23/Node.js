349b2691df2757c3de751515b223a6bd0dca4286
sibbe23 <121551871+sibbe23@users.noreply.github.com>
2023-02-06 17:55:49 +0530

    Add files via upload

--- firstjs.js
+++ firstjs.js
@@ -1,2 +1,18 @@
-const fs = require('fs');
-fs.writeFileSync('Hello.txt','Hello from Node.js')
+// const arr = ['apple', 'orange', '', 'mango', '','','lemon'];
+
+
+// console.log(arr.map(fruit => fruit === '' ? 'empty_string' : fruit));
+
+
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 4000;
+
+const server = http.createServer((req, res) => {
+  res.end('Ram');
+});
+
+server.listen(port, hostname, () => {
+  console.log('Ram');
+});
