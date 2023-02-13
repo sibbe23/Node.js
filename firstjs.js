ecb407a80d99179696958033024b977891b40873
sibbe23 <121551871+sibbe23@users.noreply.github.com>
2023-02-13 15:46:57 +0530

    Add files via upload

--- firstjs.js
+++ firstjs.js
@@ -89,15 +89,25 @@
 
 
 const express = require('express')
+
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log("This is Middleware")
-    next();
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use('/add-product',(req,res,next)=>{
+    res.send('<form action ="/product" method = "POST"><input type="text" name="title"><input type="text" name="size"><button type = "submit">Add product</button></form>')
 })
 
-app.use((req,res,next)=>{
-    console.log("This is 2nd Middleware")
-    res.send({ key1: 'value' })
+app.post('/product',(req,res,next)=>{
+    const obj = JSON.parse(JSON.stringify(req.body));
+    console.log(obj);
+    res.redirect('/')
 })
+
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from Express</h1>')
+})
+
 app.listen(5000)
